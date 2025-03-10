d14a812472ec01013da22f63e627ac9ffa1ea6e6
vikasrajpoot30 <vikas9rajpoot@gmail.com>
2025-03-10 15:23:17 +0530

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/ubuntu";
 import "@fontsource/ubuntu/400.css";
